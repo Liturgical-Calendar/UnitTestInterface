f82a260d908c10aec7eb38183176aed438b404f3
John R. D'Orazio <priest@johnromanodorazio.com>
2024-04-29 05:55:25 +0200

    start to create admin interface

---
+++ admin.php
@@ -0,0 +1,130 @@
+<?php
+
+include_once("credentials.php");
+
+function authenticated() {
+    if ( !isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ) return false;
+    if ($_SERVER['PHP_AUTH_USER'] === AUTH_USERNAME && password_verify($_SERVER['PHP_AUTH_PW'], AUTH_PASSWORD)) return true;
+    return false;
+}
+
+if(!authenticated()) {
+    header("WWW-Authenticate: Basic realm=\"Please insert your credentials\"");
+    header($_SERVER["SERVER_PROTOCOL"]." 401 Unauthorized");
+    echo "You need a username and password to access this service.";
+    die();
+}
+?><!DOCTYPE html>
+<head>
+    <title>Administration tools</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+    <!-- Custom fonts for this template-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template-->
+    <link href="https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin@7.0.5/dist/css/styles.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/1.1.1/css/bootstrap-multiselect.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <!-- Topbar -->
+    <nav class="sb-topnav navbar navbar-expand navbar-light bg-white shadow">
+        <!-- Navbar Brand -->
+        <a class="navbar-brand ps-3" href="/">Navbar</a>
+        <!-- Sidebar Toggle (Topbar) -->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle"><i class="fas fa-bars"></i></button>
+
+        <!-- Topbar Navbar -->
+        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <li class="nav-item ms-2 active" id="topNavBar_API"><a class="nav-link btn btn-outline-light border-0 fw-bold" href="./admin.php">Unit Tests Admin</a></li>
+        </ul>
+        <a class="btn btn-outline-light text-dark border-0 me-2"
+            href="https://github.com/Liturgical-Calendar/" target="_blank"
+            title="See the project repositories on GitHub">
+            <i class="fab fa-github"></i>
+        </a>
+    </nav>
+
+
+    <!-- Page Wrapper -->
+    <div id="layoutSidenav">
+
+        <div id="layoutSidenav_nav">
+
+            <!-- Sidebar -->
+            <nav class="sb-sidenav accordion sb-sidenav-dark bg-gradient" id="accordionSidebar">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <!-- Sidebar - Brand -->
+                        <div class="text-center lh-2 px-5 pt-2 sidebar-brand">
+                            <a class="text-uppercase fs-6 fw-bold text-white text-decoration-none" href="/">
+                                <?php echo _( "Catholic Liturgical Calendar" ); ?>
+                            </a>
+                        </div>
+                        <!-- <hr> -->
+                        <a class="nav-link active" href="/">
+                            <i class="sb-nav-link-icon fas fa-fw fa-cross"></i>
+                            <span><?php echo _( "Unit Test Admin" ); ?></span>
+                        </a>
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <!-- Sidebar Toggler (Sidebar) -->
+                    <div class="text-center">
+                        <button type="button" class="btn btn-secondary rounded-circle border-0 sidebarToggle" id="sidebarToggleB"><i class="fas fa-angle-left"></i></button>
+                    </div>
+                </div>
+
+            </nav>
+        </div>
+        <!-- End of Sidebar (layoutSidenav_nav) -->
+
+        <!-- Content Wrapper -->
+        <div id="layoutSidenav_content" class="pt-4">
+
+            <!-- Main Content -->
+            <main>
+                <div class="container-fluid px-4">
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </main>
+            <!-- End of Main Content -->
+
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; John D'Orazio 2020</span>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- jQuery-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.2.1/js.cookie.min.js"></script>
+
+    <!-- Bootstrap / sb-admin JavaScript-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin@7.0.5/dist/js/scripts.js"></script>
+
+    <!-- i18next -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/i18next/21.6.6/i18next.min.js" integrity="sha512-3CUvxyR4WtlZanN/KmorrZ2VALnUndAQBYjf1HEYNa6McBY+G2zYq4gOZPUDkDtuk3uBdQIva0Lk89hYQ9fQrA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-i18next/1.2.1/jquery-i18next.min.js" integrity="sha512-79RgNpOyaf8AvNEUdanuk1x6g53UPoB6Fh2uogMkOMGADBG6B0DCzxc+dDktXkVPg2rlxGvPeAFKoZxTycVooQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/i18next-http-backend@1.3.1/i18nextHttpBackend.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/1.1.2/js/bootstrap-multiselect.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+</body>
