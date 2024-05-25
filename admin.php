d0519f46c4f62b35258acb36244b0700d7376a2c
John D'Orazio <priest@johnromanodorazio.com>
2024-05-25 03:55:55 +0200

    add response alert on put request

--- admin.php
+++ admin.php
@@ -101,7 +101,7 @@ include_once 'layout/sidebar.php';
 </main>
 <!-- End of Main Content -->
 <?php
-[ "LitCalAllFestivities" => $FestivityCollection ] = json_decode( file_get_contents( "https://litcal.johnromanodorazio.com/api/dev/LitCalAllFestivities.php?locale=" . $i18n->locale ), true );
+[ "LitCalAllFestivities" => $LitCalAllFestivities ] = json_decode( file_get_contents( "https://litcal.johnromanodorazio.com/api/dev/LitCalAllFestivities.php?locale=" . $i18n->locale ), true );
 include_once 'components/NewTestModal.php';
 ?>
 <div class="modal fade" id="modalAddEditComment" tabindex="-1" aria-labelledby="addEditCommentModalLabel" aria-hidden="true">
@@ -125,14 +125,18 @@ include_once 'components/NewTestModal.php';
 </div>
 
 <!-- Warning alert for attempts at pasting scripted material in contenteditables -->
-<div class="alert alert-danger d-flex align-items-center position-absolute top-50 start-50 d-none" role="alert" id="noScriptedContentAlert">
+<div class="alert alert-danger d-flex align-items-center position-fixed top-50 start-50 d-none" role="alert" id="noScriptedContentAlert">
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
         <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
     </svg>
     <div>Hey! No script or html allowed here!</div>
 </div>
+<div class="alert alert-success d-flex align-items-center position-fixed top-50 start-50 d-none" role="alert" id="responseToPutRequest">
+    <i class="fas fa-circle-check me-2"></i>
+    <div id="responseMessage"></div>
+</div>
 <?php
-$LitEventKeys = json_encode($FestivityCollection);
-echo "<script>const LitCalTests = Object.freeze($response); const FestivityCollection = Object.freeze($LitEventKeys);</script>";
+$LitEventKeys = json_encode($LitCalAllFestivities);
+echo "<script>const LitCalTests = Object.freeze($response); let LitCalAllFestivities = Object.freeze($LitEventKeys);</script>";
 include_once 'layout/footer.php';
 ?>
