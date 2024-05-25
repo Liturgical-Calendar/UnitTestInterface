c2361155d1915b21572c4eda3e8ecb1647f1812e
John D'Orazio <priest@johnromanodorazio.com>
2024-05-25 03:53:10 +0200

    dynamic appliesTo text

--- assets/js/AssertionsBuilder.js
+++ assets/js/AssertionsBuilder.js
@@ -38,6 +38,7 @@ class AssertionsBuilder {
     // set some default initial values
     static testType     = TestType.ExactCorrespondence;
     static yearSince    = null; // only useful in case of TestType.ExactCorrespondenceSince
+    static appliesTo    = "Universal Roman Calendar";
     static bgColor      = 'bg-success';
     static txtColor     = 'text-dark';
 
@@ -78,9 +79,9 @@ class AssertionsBuilder {
             //unfortunately Firefox does not implement the "plaintext-only" value for [contenteditable], so we won't use it yet
             assertionBuildStr += `<div class="d-flex flex-column col-2 border${idy===0 || idy % 5 === 0 ? ' offset-1' : ''}">
                 <p class="text-center mb-0 fw-bold testYear">${assertion.year}</p>
-                <p class="text-center mb-0 bg-secondary text-white"><span class="me-2 fw-bold text-center">Applies to: </span><span>Universal Roman Calendar</span></p>
+                <p class="text-center mb-0 bg-secondary text-white"><span class="me-2 fw-bold text-center">Applies to: </span><span>${AssertionsBuilder.appliesTo}</span></p>
                 <div class="d-flex justify-content-between align-items-center ps-2 pe-1 border-bottom ${AssertionsBuilder.bgColor} ${AssertionsBuilder.txtColor}" style="min-height:3em;"><span class="me-2 fw-bold w-25">ASSERT THAT: </span><span class="ms-2 text-end assert">${assertion.assert}</span><span role="button" class="btn btn-xs btn-danger ms-1 toggleAssert"><i class="fas fa-repeat"></i></span></div>
-                <div class="d-flex justify-content-between align-items-center ps-2 pe-2 ${AssertionsBuilder.bgColor} ${AssertionsBuilder.txtColor}" style="min-height:3em;"><span class="me-2 fw-bold w-25">EXPECT VALUE: </span><span class="ms-2 expectedValue" data-value="${assertion.expectedValue}">${expectedDateStr}</span></div>
+                <div class="d-flex justify-content-between align-items-center ps-2 pe-1 ${AssertionsBuilder.bgColor} ${AssertionsBuilder.txtColor}" style="min-height:3em;"><span class="me-2 fw-bold w-25">EXPECT VALUE: </span><span class="ms-2 expectedValue" data-value="${assertion.expectedValue}">${expectedDateStr}</span><span role="button" class="btn btn-xs btn-danger ms-1 editDate"><i class="fas fa-pen-to-square"></i></span></div>
                 <div class="flex-grow-1 d-flex flex-column text-white p-3" style="background-color: cadetblue;"><span class="fw-bold">ASSERTION:${commentStr}</span><span contenteditable>${assertion.assertion}</span></div>
                 </div>`;
         });
