2481649c930e3a0761dee76b7eff8ed8494e0fb5
John R. D'Orazio <priest@johnromanodorazio.com>
2024-09-16 21:04:54 +0200

    admin users now an array

--- includes/auth.php
+++ includes/auth.php
@@ -1,13 +1,25 @@
 <?php
 
+if (false === file_exists("credentials.php")) {
+    die("missing credentials definition");
+}
+
 include_once("credentials.php");
+
+if (false === defined("AUTH_USERS")) {
+    die("missing AUTH_USERS definition");
+}
+
+if (false === is_array(AUTH_USERS) || 0 === count(AUTH_USERS)) {
+    die("AUTH_USERS must be an array");
+}
 
 function authenticated()
 {
     if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
         return false;
     }
-    if ($_SERVER['PHP_AUTH_USER'] === AUTH_USERNAME && password_verify($_SERVER['PHP_AUTH_PW'], AUTH_PASSWORD)) {
+    if (array_key_exists($_SERVER['PHP_AUTH_USER'], AUTH_USERS) && password_verify($_SERVER['PHP_AUTH_PW'], AUTH_USERS[$_SERVER['PHP_AUTH_USER']])) {
         return true;
     }
     return false;
