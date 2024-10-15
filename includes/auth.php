c626294aec44b404e6d679fd156dd519bc8efd35
John R. D'Orazio <priest@johnromanodorazio.com>
2024-10-15 17:32:57 +0200

    suppress intelephense errors

--- includes/auth.php
+++ includes/auth.php
@@ -10,15 +10,22 @@ if (false === defined("AUTH_USERS")) {
     die("missing AUTH_USERS definition");
 }
 
+/** @disregard P1011 since AUTH_USERS is defined in credentials.php */
 if (false === is_array(AUTH_USERS) || 0 === count(AUTH_USERS)) {
     die("AUTH_USERS must be an array");
 }
 
+/**
+ * Verifies if the user is authenticated.
+ *
+ * @return boolean True if the user is authenticated, false otherwise.
+ */
 function authenticated()
 {
     if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
         return false;
     }
+    /** @disregard P1011 since AUTH_USERS is defined in credentials.php */
     if (array_key_exists($_SERVER['PHP_AUTH_USER'], AUTH_USERS) && password_verify($_SERVER['PHP_AUTH_PW'], AUTH_USERS[$_SERVER['PHP_AUTH_USER']])) {
         return true;
     }
