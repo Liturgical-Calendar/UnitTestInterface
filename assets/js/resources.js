d2d5d1fc42dec03ea8a4f70e3c65be0bf49d96d8
John R. D'Orazio <priest@johnromanodorazio.com>
2024-11-29 15:32:48 +0100

    better display for block titles

--- assets/js/resources.js
+++ assets/js/resources.js
@@ -275,7 +275,7 @@ const resourcePaths = {
  * @returns {string} The HTML template for the resource.
  */
 const resourceTemplate = (resource, idx) => `<div class="col-1 ${idx === 0 || idx % 11 === 0 ? 'offset-1' : ''}">
-    <p class="text-center mb-0 bg-secondary text-white h-25"><span title="${resourcePaths[resource]}">${resourcePaths[resource]}</span></p>
+    <div class="text-center mt-2 mb-0 bg-secondary text-white h-25"><span title="${resourcePaths[resource]}" class="text-break d-inline-block w-75">${resourcePaths[resource]}</span></div>
     <div class="card text-white bg-info rounded-0 ${resource} file-exists">
         <div class="card-body">
             <p class="card-text d-flex justify-content-between"><span><svg class="svg-inline--fa fa-circle-question fa-fw" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle-question" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"></path></svg><!-- <i class="fas fa-circle-question fa-fw"></i> Font Awesome fontawesome.com --> data exists</span></p>
@@ -294,7 +294,7 @@ const resourceTemplate = (resource, idx) => `<div class="col-1 ${idx === 0 || id
 </div>`;
 
 const sourceTemplate = (sourceItem, idx) => `<div class="col-1 ${idx === 0 || idx % 11 === 0 ? 'offset-1' : ''}">
-<p class="text-center mb-0 bg-secondary text-white h-25"><span title="${sourceItem.sourceFile ?? sourceItem.sourceFolder}">${sourceItem.validate}</span></p>
+<div class="text-center mt-2 mb-0 bg-secondary text-white h-25"><span title="${sourceItem.sourceFile ?? sourceItem.sourceFolder}" class="text-break d-inline-block w-75">${sourceItem.validate}</span></div>
 <div class="card text-white bg-info rounded-0 ${sourceItem.validate} file-exists">
     <div class="card-body">
         <p class="card-text d-flex justify-content-between"><span><svg class="svg-inline--fa fa-circle-question fa-fw" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle-question" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"></path></svg><!-- <i class="fas fa-circle-question fa-fw"></i> Font Awesome fontawesome.com --> data exists</span></p>
